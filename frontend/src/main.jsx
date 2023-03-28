d28676c18ffc216c8ef8882ad53764c8910b9b5e
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-28 17:38:14 -0400

    Adds routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {createBrowserRouter, createRoutesFromElements, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route element = {<Header />}>
+        <Route element={<HomePage />} path="/" loader={ () => true } />
+        <Route element = {<LoginPage />} path = "/login" />
+        <Route element={<SignUpPage />} path="/sign-up" />
+        <Route element={<QuestionPage />} path="/question/:pk" />
+        <Route element={<UserPage />} path="/user/:pk" />
+      </Route>
+    </>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
