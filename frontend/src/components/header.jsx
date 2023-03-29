cc53872c960b7e9d6fc4c1949ae9d752b3840b18
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-29 15:41:58 -0400

    Formats header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,8 @@
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faMessageQuestion } from '@fortawesome/free-solid-svg-icons'
-import './styles/header.css'
+import { faInbox } from '@fortawesome/free-solid-svg-icons'
+import '../styles/header.css'
+import { Outlet } from 'react-router-dom'
 export default function Header() {
 
   return (
@@ -10,8 +11,19 @@ export default function Header() {
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex lg:flex-1">
             <a href="" className="-m-1.5 p-1.5">
-              <FontAwesomeIcon icon={faMessageQuestion} />
-              <h1 className='title'>QuestionBox</h1>
+              <h1 className='title'>
+              <FontAwesomeIcon icon={faInbox} />
+               &nbsp;QuestionBox</h1>
+            </a>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              Log in <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <a href="#" className="text-xl font-semibold leading-6 text-gray-900">
+              Sign-Up <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
         </nav>
