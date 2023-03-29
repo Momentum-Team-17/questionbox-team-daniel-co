cc7fcb0de9deebc1bf3dc4544270d1af32ee58bb
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-29 11:58:49 -0400

    added question listcreate view with isauthenticated permissions on create

--- backend/questionbox/views.py
+++ backend/questionbox/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 
 # Create your views here.
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    @permission_classes(IsAuthenticated)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
