7e7550332b50f8086feb5c6ea8022aaf8531ca6b
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-29 14:58:50 -0400

    Adds FA & Tittle

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,25 @@
 
-
-
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faMessageQuestion } from '@fortawesome/free-solid-svg-icons'
+import './styles/header.css'
 export default function Header() {
 
-  return (null)
+  return (
+    <>
+      <header className="bg-white">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div className="flex lg:flex-1">
+            <a href="" className="-m-1.5 p-1.5">
+              <FontAwesomeIcon icon={faMessageQuestion} />
+              <h1 className='title'>QuestionBox</h1>
+            </a>
+          </div>
+        </nav>
+
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
 }
