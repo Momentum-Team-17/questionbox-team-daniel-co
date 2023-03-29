7d92effc0d2fbb9c0960c7c1532cb7d316230ced
Stinacon <122473348+Stinacon@users.noreply.github.com>
2023-03-29 12:25:09 -0400

    trying to fix permissions on create function of question list view

---
+++ backend/questionbox/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from questionbox import views
+
+
+urlpatterns = [
+    path('', views.QuestionList.as_view()),
+]
--- backend/questionbox/views.py
+++ backend/questionbox/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, filters
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
+from .models import User, Question, Answer, StarTracker
+from .serializers import QuestionSerializer
 
 
 # Create your views here.
@@ -9,6 +11,8 @@ class QuestionList(generics.ListCreateAPIView):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
 
-    @permission_classes(IsAuthenticated)
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return []
+        else:
+            return [IsAuthenticated()]
