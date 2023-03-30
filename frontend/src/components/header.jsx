3a2a39cba41402c471f298b220318dc9a86e9771
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-30 09:05:34 -0400

    Finishes off basic header
    Needs to conditionally render login vs profile dropdown, mobile, etc

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -9,20 +9,20 @@ export default function Header() {
     <>
       <header className="bg-white">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <a href="" className="-m-1.5 p-1.5">
-              <h1 className='title'>
+          <div className="flex sm:flex-1">
+            <a href="" className="-m-1.5 p-1.5 grow">
+              <h1 className='title font-semibold'>
               <FontAwesomeIcon icon={faInbox} />
                &nbsp;QuestionBox</h1>
             </a>
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <div className="hidden sm:flex sm:flex-none sm:justify-end mx-4">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
-                    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-xl font-semibold leading-6 text-gray-900">
+                    <div className="hidden sm:flex sm:flex-none sm:justify-end">
+            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
               Sign-Up <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
