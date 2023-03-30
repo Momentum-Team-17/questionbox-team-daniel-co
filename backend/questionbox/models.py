728195cf841cb7c518823f0186f173fdd6e97f77
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-30 10:49:46 -0400

    added is_accepted to AnswerModel and accepted_answer to QuestionModel

--- backend/questionbox/models.py
+++ backend/questionbox/models.py
@@ -14,9 +14,19 @@ class Question(models.Model):
     text = models.TextField(max_length=5000)
     author = models.ForeignKey(to='User', on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
+    # accepted_answer = models.OneToOneField(
+    #     to='Answer', on_delete=models.CASCADE, blank=True, null=True, related_name='accepted_for')
 
     class Meta:
         ordering = ['-time_created']
+
+    @property
+    def accepted_answer(self):
+        accepted_answer = None
+        for answer in self.answers.all():
+            if answer.is_accepted:
+                accepted_answer = answer
+        return accepted_answer
 
     def __str__(self):
         return f'{self.title} asked by {self.author}'
@@ -28,6 +38,7 @@ class Answer(models.Model):
     text = models.TextField(max_length=5000)
     author = models.ForeignKey(to='User', on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
+    is_accepted = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['-time_created']
