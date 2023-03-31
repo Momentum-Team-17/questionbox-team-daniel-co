de99f944d916d5a38ed5d2548b2f9a1c2a612024
chriscors <70537376+chriscors@users.noreply.github.com>
2023-03-31 15:45:13 -0400

    Adds error

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -28,6 +28,9 @@ export default function App() {
   loadHome = () => {
     axios.get(URL)
       .then((response) => {
+        if (response.status === 404) {
+          throw new Response("Not Found", { status: 404 });
+        }
         setHomeData(response.data)
         return response.data
       })
@@ -39,6 +42,9 @@ export default function App() {
     } else {
       axios.get(`${URL}/question/${pk}`)
         .then((response) => {
+          if (response.status === 404) {
+            throw new Response("Not Found", { status: 404 });
+          }
           return response.data
         })
     }
@@ -54,7 +60,7 @@ export default function App() {
   const router = createBrowserRouter(  
       createRoutesFromElements(
         <>
-          <Route element={<Header token={token} username={ username } />}>
+          <Route element={<Header token={token} username={username} errorElement={ <><h1>Error!</h1><HomePage /></>} />}>
             <Route element={<HomePage token={token} />} path="/" loader={ loadHome } />
             <Route element={<QuestionPage token={token} loader={(pk) => loadQuestion({ pk })} />} path="/question/:pk" /> //save yourself the API call and just filter to the pk of the main call as long as you have one ?? I think that makes sense
             <Route element={<UserPage token={token} username={username} loader={ (username) => loadUser({username})} />} path="/user/:username" />
