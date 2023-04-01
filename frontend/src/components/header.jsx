6103b4f0c5e226a68ff7961fadac2a1ea7550f3c
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-01 09:56:46 -0400

    Adds link to home

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,7 +2,7 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faInbox } from '@fortawesome/free-solid-svg-icons'
 import '../styles/header.css'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 export default function Header() {
 
   return (
@@ -10,11 +10,11 @@ export default function Header() {
       <header className="bg-white">
         <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
           <div className="flex sm:flex-1">
-            <a href="" className="-m-1.5 p-1.5 grow">
+            <Link to="/" className="-m-1.5 p-1.5 grow">
               <h1 className='title font-semibold'>
               <FontAwesomeIcon icon={faInbox} />
                &nbsp;QuestionBox</h1>
-            </a>
+            </Link>
           </div>
           <div className="hidden sm:flex sm:flex-none sm:justify-end mx-4">
             <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
