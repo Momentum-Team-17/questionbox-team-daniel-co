7d2fd20fa1cc044c389707d98498f315953e6ffc
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-01 00:58:57 -0400

    All loaders working (minus user)

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
@@ -16,7 +16,7 @@ import axios from 'axios'
 export default function App() {
   const [token, setToken] = useLocalStorageState('token')
   const [username, setUserName] = useState()
-  const [homeData, setHomeData] = useState()
+  const homeData = useRef()
 
   const URL = 'https://questionbox-mgxz.onrender.com/'
 
@@ -25,36 +25,37 @@ export default function App() {
     setUserName(username);
   }
 
-  loadHome = () => {
-    axios.get(URL)
-      .then((response) => {
-        if (response.status === 404) {
-          throw new Response("Not Found", { status: 404 });
-        }
-        setHomeData(response.data)
-        return response.data
-      })
-  }
+  const loadHome = async () => {
+    const res = await axios.get(URL)
+    homeData.current = (res.data)
 
-  loadQuestion = (pk) => {
-    if (homeData) {
-      return homeData.filter((q) => q.pk === pk)
-    } else {
-      axios.get(`${URL}/question/${pk}`)
-        .then((response) => {
-          if (response.status === 404) {
-            throw new Response("Not Found", { status: 404 });
-          }
-          return response.data
-        })
+    if (res.status === 404) {
+      throw new Response("Not Found", { status: 404 });
     }
+    return res
   }
 
-  loadUser = (username) => {
-    axios.get(`${URL}/user/${username}`)
-      .then((response) => {
-        return response.data
-      })
+  const loadQuestion = async ({params}) => {
+    console.log(params.params.pk);
+    if (homeData.current) {
+      //console.log(homeData.results.filter((q) => q.pk === pk));
+      return homeData.results.filter((q) => q.pk === pk)
+    }
+    
+    const res = await axios.get(`${URL}questions/${params.params.pk}`)
+    if (res.status === 404) {
+      throw new Response("Not Found", { status: 404 });
+    }
+    return res
+  }
+  
+
+  const loadUser = async (params) => {
+    const res = await axios.get(`${URL}users/${params.username}`)
+    if (res.status === 404) {
+      throw new Response("Not Found", { status: 404 });
+    }
+    return res
   }
 
   const router = createBrowserRouter(  
@@ -62,8 +63,8 @@ export default function App() {
         <>
           <Route element={<Header token={token} username={username} errorElement={ <><h1>Error!</h1><HomePage /></>} />}>
             <Route element={<HomePage token={token} />} path="/" loader={ loadHome } />
-            <Route element={<QuestionPage token={token} loader={(pk) => loadQuestion({ pk })} />} path="/question/:pk" /> //save yourself the API call and just filter to the pk of the main call as long as you have one ?? I think that makes sense
-            <Route element={<UserPage token={token} username={username} loader={ (username) => loadUser({username})} />} path="/user/:username" />
+            <Route element={<QuestionPage token={token} />}loader={(params)=>loadQuestion({params})} path="/question/:pk" /> //save yourself the API call and just filter to the pk of the main call as long as you have one ?? I think that makes sense
+            <Route element={<UserPage token={token} username={username} />} loader={ (params) => loadUser({params})} path="/user/:username" />
             <Route element={<LoginPage setAuth={ setAuth } />} path = "/login" />
             <Route element={<SignUpPage />} path="/sign-up" />
           </Route>
--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,7 +1,17 @@
-
+import { useLoaderData } from "react-router-dom";
 
 
 export default function HomePage() {
+  const data = useLoaderData()
+  console.log(data)
+  return <h1>CHILL</h1>
+  return (data.map((q) => <Card data={ data } />))
+}
 
-  return null
+function Card({ data }) {
+  return (
+    <>
+      
+    </>
+  )
 }
