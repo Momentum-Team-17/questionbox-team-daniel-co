e2fad8233f93aa6c166eafb7a0339bce26f513ab
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-01 09:56:34 -0400

    Adds 404 to routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -10,6 +10,7 @@ import Header from './components/header'
 import QuestionPage from './routes/question'
 import useLocalStorageState from 'use-local-storage-state'
 import axios from 'axios'
+import Page404 from './routes/404'
 
 
 
@@ -61,12 +62,13 @@ export default function App() {
   const router = createBrowserRouter(  
       createRoutesFromElements(
         <>
-          <Route element={<Header token={token} username={username} errorElement={ <><h1>Error!</h1><HomePage /></>} />}>
+          <Route element={<Header token={token} username={username} errorElement={ Page404 } />}>
             <Route element={<HomePage token={token} />} path="/" loader={ loadHome } />
             <Route element={<QuestionPage token={token} />}loader={(params)=>loadQuestion(params)} path="/question/:pk" /> //save yourself the API call and just filter to the pk of the main call as long as you have one ?? I think that makes sense
             <Route element={<UserPage token={token} username={username} />} loader={ (params) => loadUser(params)} path="/user/:username" />
             <Route element={<LoginPage setAuth={ setAuth } />} path = "/login" />
             <Route element={<SignUpPage />} path="/sign-up" />
+            <Route element={<Page404 />} path="/*" />
           </Route>
         </>
       )
