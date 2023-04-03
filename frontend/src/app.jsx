13cbdf5723aef4efc595787e66518995644f4185
chriscors <70537376+chriscors@users.noreply.github.com>
2023-04-03 14:18:58 -0400

    Adds global login states

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -17,6 +17,8 @@ import Page404 from './routes/404'
 export default function App() {
   const [token, setToken] = useLocalStorageState('token')
   const [username, setUserName] = useState()
+  const [isLoginOpen, setIsLoginOpen] = useState(false)
+  const [isSignupOpen, setIsSignupOpen] = useState(false)
   const homeData = useRef()
 
   const URL = 'https://questionbox-mgxz.onrender.com'
@@ -25,6 +27,7 @@ export default function App() {
     setToken(token);
     setUserName(username);
   }
+
 
   const loadHome = async () => {
     const res = await axios.get(URL)
@@ -57,7 +60,8 @@ export default function App() {
   const router = createBrowserRouter(  
       createRoutesFromElements(
         <>
-          <Route element={<Header token={token} username={username} errorElement={ Page404 } />}>
+          <Route element={<Header token={token} username={username} errorElement={Page404} setAuth={setAuth}
+            isLoginOpen={isLoginOpen} setIsLoginOpen={setIsLoginOpen} isSignupOpen={ isSignupOpen } setIsSignupOpen={ setIsSignupOpen} />}>
             <Route element={<HomePage token={token} />} path="/" loader={ loadHome } />
             <Route element={<QuestionPage token={token} />}loader={(params)=>loadQuestion(params)} path="/question/:pk" /> //save yourself the API call and just filter to the pk of the main call as long as you have one ?? I think that makes sense
             <Route element={<UserPage token={token} username={username} />} loader={ (params) => loadUser(params)} path="/user/:username" />
