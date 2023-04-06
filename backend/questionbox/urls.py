cda667840eac984d366da4abc506c44d26ad2670
Jeeglobs <dp.hudgins@hotmail.com>
2023-04-05 22:34:27 -0400

    reorganized urls.py

--- backend/questionbox/urls.py
+++ backend/questionbox/urls.py
@@ -3,20 +3,31 @@ from questionbox import views
 
 
 urlpatterns = [
+    # GET
     path('', views.QuestionList.as_view()),
-    path('questions', views.CreateQuestion.as_view()),
-    path('questions/<int:pk>/answers', views.CreateAnswer.as_view()),
-    path('questions/<int:pk>', views.QuestionDetails.as_view()),
-    path('answers/<int:pk>', views.AnswerDetails.as_view()),
-    path('users/questions', views.ListUserQuestions.as_view()),
-    path('users/answers', views.ListUserAnswers.as_view()),
-    path('search', views.QuestionSearch.as_view(), name='question-search'),
-    path('profile/<slug:username>', views.UserDetailView.as_view()),
-    path('questions/favorite', views.favorite_question),
-    path('answers/favorite', views.favorite_answer),
-    path('answers/<int:pk>/accepted', views.AcceptAnswer.as_view()),
-    path('users/fav-questions', views.ListFavoriteQuestions.as_view()),
-    path('users/fav-answers', views.ListFavoriteAnswers.as_view()),
     path('questions/answered', views.AnsweredQuestions.as_view()),
     path('questions/unanswered', views.UnansweredQuestions.as_view()),
+    path('search', views.QuestionSearch.as_view(), name='question-search'),
+
+    # POST
+    path('questions', views.CreateQuestion.as_view()),
+    path('questions/<int:pk>/answers', views.CreateAnswer.as_view()),
+
+    # PATCH
+    path('answers/<int:pk>/accepted', views.AcceptAnswer.as_view()),
+    path('questions/favorite', views.favorite_question),
+    path('answers/favorite', views.favorite_answer),
+
+    # GET
+    path('users/fav-questions', views.ListFavoriteQuestions.as_view()),
+    path('users/fav-answers', views.ListFavoriteAnswers.as_view()),
+    path('users/questions', views.ListUserQuestions.as_view()),
+    path('users/answers', views.ListUserAnswers.as_view()),
+
+    # GET | PUT | PATCH | DELETE
+    path('questions/<int:pk>', views.QuestionDetails.as_view()),
+    path('answers/<int:pk>', views.AnswerDetails.as_view()),
+
+    # GET
+    path('profile/<slug:username>', views.UserDetailView.as_view()),
 ]
